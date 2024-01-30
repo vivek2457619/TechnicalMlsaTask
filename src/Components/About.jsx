f05e71192513a96c6930fd33244646440d56ca37
Vivek <125206906+vivek2457619@users.noreply.github.com>
2024-01-30 17:02:12 +0530

    Add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/laptop-working-place-image.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Beautiful working place background ";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Passionate Web Developer and nthusiastically motivated to provide valuable contributions and actively participate in the operations of an organization.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web Developer",
+  "MERN Stack Developer",
+  "React based webapps",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Beautiful design",
+  "Security matters",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Passionate about the endless possibilities of technology, I am an accomplished developer and programmer well-versed in HTML5, CSS3, and JavaScript. My journey in the world of programming has been a relentless pursuit of excellence, with a keen focus on mastering the intricacies of the MERN (MongoDB, Express.js, React, Node.js) stack. ";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/books-image.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Books and pen image";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Linked Access",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is my Linkedin account where you can contact me and updated with latest technology & trends.",
+    url: "https://www.linkedin.com/in/vivek-mittal-379a97249",
   },
   {
-    title: "Web Development for Beginners",
+    title: "GitHub Projects",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is my GitHub account where you can access the latest projects and important repositries.",
+    url: "https://github.com/vivek2457619",
   },
   {
-    title: "My Resume Site",
+    title: "TextTutils-React App Project",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This is a React app where you can modify and customize your text. It provides lots of functionalities.",
+    url: "https://vivek2457619.github.io/TextTutils-React/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Login-and-Registeration-form-MERN-Project",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is a MERN Stack project. It is a login and registration form made up of pure authentication and security with SSL key of passwords and user credentials",
+    url: "https://github.com/vivek2457619/Login-and-Registeration-form-MERN-Project",
   },
 ];
 
